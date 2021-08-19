b160495b3348fed2da57bfce41f02d189d709566
Young wook Jeon <ela87@naver.com>
2021-08-19 21:08:30 +0900

    Setup a RSocket project with Spring

---
+++ spring-rsocket/src/main/java/io/young/dev/springrsocket/SpringRsocketApplication.java
@@ -0,0 +1,13 @@
+package io.young.dev.springrsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRsocketApplication.class, args);
+	}
+
+}
---
+++ spring-rsocket/src/main/java/io/young/dev/springrsocket/service/MathService.java
@@ -0,0 +1,34 @@
+package io.young.dev.springrsocket.service;
+
+import io.young.dev.springrsocket.dto.ChartResponseDto;
+import io.young.dev.springrsocket.dto.ComputationRequestDto;
+import io.young.dev.springrsocket.dto.ComputationResponseDto;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+public class MathService {
+
+    public Mono<Void> print(Mono<ComputationRequestDto> requestDtoMono) {
+        return requestDtoMono
+                .doOnNext(System.out::println)
+                .then();
+    }
+
+    public Mono<ComputationResponseDto> findSquare(Mono<ComputationRequestDto> requestDtoMono) {
+        return requestDtoMono
+                .map(ComputationRequestDto::getInput)
+                .map(i -> new ComputationResponseDto(i, i * i));
+    }
+
+    public Flux<ComputationResponseDto> tableStream(ComputationRequestDto dto) {
+        return Flux.range(1, 10)
+                .map(i -> new ComputationResponseDto(dto.getInput(), dto.getInput() * i));
+    }
+
+    public Flux<ChartResponseDto> chartStream(Flux<ComputationRequestDto> dto) {
+        return dto.map(ComputationRequestDto::getInput)
+                .map(i -> new ChartResponseDto(i, (i * i) + 1));
+    }
+}
